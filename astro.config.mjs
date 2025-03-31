a594bbc5f0f754f4be8470d279131fc23c6ce028
rzvbka <wikstef2@gmail.com>
2025-03-31 13:56:48 +0200

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    name: 'force-utf8',
+    enforce: 'pre',
+    transform(code) {
+      return Buffer.from(code, 'utf8').toString();
+    }
+  },
+  site: 'https://rvabka.github.io',
+  base: 'koparkaPage'
+});
